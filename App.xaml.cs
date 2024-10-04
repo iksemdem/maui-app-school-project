ab52a5e774f955c22e02ca0ff5f14147c7f45fc7
Egzamin <Egzamin@TE-CZE-D-01S012>
2024-10-04 12:40:13 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace miau_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace miau_app
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNameTextChanged(object sender, EventArgs ev)
+        {
+            //newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+        }
+
+        private void OnNameCompleted(object sender, EventArgs ev)
+        {
+            //newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+        }
+
+        private void OnSurnameTextChanged(object sender, EventArgs ev)
+        {
+            //newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+        }
+        private void OnSurnameCompleted(object sender, EventArgs ev)
+        {
+            //newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+        }
+
+        private void OnPhoneNumberTextChanged(object sender, EventArgs ev)
+        {
+            //newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+        }
+        private void OnPhoneNumberCompleted(object sender, EventArgs ev)
+        {
+            //newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+        }
+        private void OnButtonSendClicked(object sender, EventArgs ev)
+        {
+            
+            newLabel.Text = $"Wprowadzone dane:\nImię: {Name.Text},\nNazwisko: {Surname.Text},\nNumer Telefonu: {PhoneNumber.Text}";
+            
+        }
+
+    }
+
+}
